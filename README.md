041db8936d917c6cc069d0e4a19dbb6c3d4f27bc
yoitsu05 <yoshitaka.oitsu@plaid.co.jp>
2020-08-06 14:19:06 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sftp2s3
+
 Synchronizing Files from SFTP Server to S3 Running on AWS Lambda
+※ only .(CSV|csv) files
---
+++ index.js
@@ -0,0 +1,35 @@
+const SSH2Promise = require("ssh2-promise");
+
+const {
+  pullTree,
+  getS3Files
+} = require("./lib/utils");
+const {
+  getSSHConfig
+} = require("./lib/sshConfig");
+const {
+  getEnv
+} = require("./lib/config");
+
+exports.handler = async (event, context) => {
+  const sshconfig = getSSHConfig();
+
+  const ssh = await new SSH2Promise(sshconfig);
+  const sftp = ssh.sftp();
+  try {
+    const s3Files = await getS3Files(getEnv("SFTP_TARGET_S3_BUCKET"));
+    await pullTree({
+      sftp,
+      dirpath: getEnv("SFTP_SOURCE_DIR"),
+      s3Files,
+      fileRetentionMilliseconds: 24 * 60 * 60 * 1000
+    });
+  } catch (err) {
+    console.error(`ERROR: ${err.message}`);
+    return err;
+  } finally {
+    await ssh.close();
+  }
+  console.log('success');
+  return;
+};
