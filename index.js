4e4b6ae659bb0ef6e3d8b2dfdc3be0135ecddf57
yoitsu05 <yoshitaka.oitsu@plaid.co.jp>
2020-08-08 10:55:42 +0900

    add: formatter

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const SSH2Promise = require("ssh2-promise");
+const SSH2Promise = require('ssh2-promise');
 
-const {
-  pullTree,
-  getS3Files
-} = require("./lib/utils");
-const {
-  getSSHConfig
-} = require("./lib/sshConfig");
-const {
-  getEnv
-} = require("./lib/config");
+const { pullTree, getS3Files } = require('./lib/utils');
+const { getSSHConfig } = require('./lib/sshConfig');
+const { getEnv } = require('./lib/config');
 
 exports.handler = async (event, context) => {
   const sshconfig = getSSHConfig();
@@ -17,12 +10,12 @@ exports.handler = async (event, context) => {
   const ssh = await new SSH2Promise(sshconfig);
   const sftp = ssh.sftp();
   try {
-    const s3Files = await getS3Files(getEnv("SFTP_TARGET_S3_BUCKET"));
+    const s3Files = await getS3Files(getEnv('SFTP_TARGET_S3_BUCKET'));
     await pullTree({
       sftp,
-      dirpath: getEnv("SFTP_SOURCE_DIR"),
+      dirpath: getEnv('SFTP_SOURCE_DIR'),
       s3Files,
-      fileRetentionMilliseconds: 24 * 60 * 60 * 1000
+      fileRetentionMilliseconds: 24 * 60 * 60 * 1000,
     });
   } catch (err) {
     console.error(`ERROR: ${err.message}`);
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: 'all',
+  htmlWhitespaceSensitivity: 'strict',
+  arrowParens: 'avoid',
+};
